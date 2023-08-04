0ef9b44b5c71ee37604205527bd3239815cc420d
Antoni Klebba <Antoni.Klebba@gmail.com>
2023-08-04 14:05:20 +0200

    Add ioc

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import { Users } from './services/users';
-import { Logger } from './services/logger';
+import { Users } from "./services/users";
+import { Logger } from "./services/logger";
+import type { User, ApiConfig } from "./types";
+import { createIoCContainer } from "./ioc/index";
 
-import type { User, ApiConfig } from './types';
+const ioc = createIoCContainer();
 
-const renderUsers = async (config: ApiConfig) => {
-  const usersService = new Users(config);
+const renderUsers = async () => {
+  // const usersService = new Users(config);
+  console.log(ioc);
+  const usersService = ioc.resolve("users");
   const users = await usersService.getUsers();
 
-  const listNode = document.getElementById('users-list');
+  const listNode = document.getElementById("users-list");
 
   users.forEach((user: User) => {
-    const listItemNode = document.createElement('li');
+    const listItemNode = document.createElement("li");
 
     listItemNode.innerHTML = user.name;
     listNode.appendChild(listItemNode);
@@ -21,13 +25,15 @@ const app = () => {
   const config = (window as any).__CONFIG__;
   delete (window as any).__CONFIG__;
 
-  renderUsers(config.api);
+  ioc.register("apiConfig", config.api);
+
+  renderUsers();
 };
 
-window.onload = (event: Event) => {
-  const logger = new Logger();
+window.onload = () => {
+  const logger = ioc.resolve("logger");
 
-  logger.info('Page is loaded.');
+  logger.info("Page is loaded.");
 
   app();
 };
